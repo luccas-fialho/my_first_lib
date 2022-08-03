7b1899567bc9002ebdd32bc2e4cede9d00eabe31
Luccas Fialho <luccas.flh@gmail.com>
2022-08-03 19:48:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// sempre que iniciar um projeto em node precisamos criar um arquivo de configuração chamado package.json com npm init
+
+console.log('Vamos começar!');
+
+const paragrafo = 'Texto retornado por uma função';
+
+function retornaTexto(string) {
+    return string
+}
+
+console.log(retornaTexto(paragrafo));
