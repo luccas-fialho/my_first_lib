cfdf27452358dc2cb42f08d649f14cc22e74fbf4
Luccas Fialho <luccas.flh@gmail.com>
2022-08-03 21:14:14 -0300

    trabalhando com arquivos locais

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 // sempre que iniciar um projeto em node precisamos criar um arquivo de configuração chamado package.json com npm init
 
-console.log('Vamos começar!');
+import chalk from 'chalk';
+import fs from 'fs';
 
-const paragrafo = 'Texto retornado por uma função';
-
-function retornaTexto(string) {
-    return string
+function dealWithError(err) {
+    throw new Error(chalk.red(err.code, 'file not found.'));
 }
 
-console.log(retornaTexto(paragrafo));
+async function getFile(path) {
+    const encoding = 'utf-8';
+    try {
+        const text = await fs.promises.readFile(path, encoding);
+        console.log(chalk.green(text));
+    } catch (error) {
+        dealWithError(error);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+    }
+}
+
+// function getFile(path) {
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(path, encoding)
+//     .then((text) => {
+//         console.log(chalk.green(text));
+//     })
+//     .catch((err) => {
+//         dealWithError(err);
+//     })
+// }
+
+// function getFile(path) {
+//     const encoding = 'utf-8';
+//     fs.readFile(path, encoding, (err, text) => {
+//         if(err) {
+//             dealWithError(err);
+//         }
+//         console.log(chalk.green(text));
+//     });
+// }
+
+getFile('./arquivos/texto1.md');
