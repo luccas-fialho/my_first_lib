dfb32d03b7a13d03116a01a3899384a062c8cb44
Luccas Fialho <luccas.flh@gmail.com>
2022-08-05 14:32:18 -0300

    Mudanças nos ignores

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const getFile = require("./index.js");
 const chalk = require('chalk');
 const checkURLs = require("./http-validacao.js");
@@ -6,7 +8,7 @@ const path = process.argv;
 
 async function textProcess(path) {
     const result = await getFile(path[2]);
-    if(path[3] === 'validar') {
+    if(path[3] === 'validate') {
         console.log(chalk.yellow('Validated links'), await checkURLs(result));
     } else {
         console.log(chalk.yellow('List of links'), result);
